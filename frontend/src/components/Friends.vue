52127dc6a3966f8a6045b79b252dac0fcd17d7bb
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-07-11 15:40:33 -0300

    v1

---
+++ frontend/src/components/Friends.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+      <ul>
+          <li>
+              <img src="/assets/logo.png">
+              <br>
+              brs
+          </li>
+          <li>
+              G
+          </li>
+      </ul>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Friends'
+
+}
+</script>
+
+<style>
+ul li{
+    display: inline-block;
+}
+</style>
