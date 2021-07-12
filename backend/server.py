87fd39b0429079e9bb14e8706837755d448f54c8
Gilson Oliveira <gilsgil@gmail.com>
2021-07-11 21:49:27 -0300

    database v1

--- backend/server.py
+++ backend/server.py
@@ -1,32 +1,46 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='../frontend/dist', static_folder='../frontend/dist/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# App Configs
+app = Flask(__name__, template_folder='../frontend/dist', static_folder='../frontend/dist/js')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/webchat'
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+io = SocketIO(app)
+db = SQLAlchemy(app)
 
 
+# Routes
 @app.route('/')
 def view():
     return render_template('index.html')
 
 
-@socketio.on('message')
+@io.on('message')
 def handle_message(message):
     print('message: ' + message)
     send(message, broadcast=True)
 
 
-@socketio.on('connect')
+@io.on('connect')
 def test_connect():
     print('Client connected')
 
 
-@socketio.on('disconnect')
+@io.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
 
+# Models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 if __name__ == '__main__':
-    socketio.run(app)
+    io.run(app)
